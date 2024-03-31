8e10e009e1f694257408f944e9fb47b4045577bc
victor <141786730+vichsweet@users.noreply.github.com>
2024-03-31 16:30:50 -0300

    Add files via upload

---
+++ atividades/atividade-03/TrabalhoBD-Meso7.sql
@@ -0,0 +1,34 @@
+-- Inserção dos municípios da microrregião de Caririaçu
+INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
+VALUES 
+('Altaneira', -6.928323, -39.57252),
+('Caririaçu', -7.047801, -39.283327),
+('Farias Brito', -6.920674, -39.564316),
+('Granjeiro', -7.145130, -39.336294);
+
+-- Inserção dos municípios da microrregião de Cariri
+INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
+VALUES 
+('Barbalha', -7.358413, -39.331816),
+('Crato', -7.288420, -39.468317),
+('Jardim', -7.581972, -39.280938),
+('Juazeiro do Norte', -7.225012, -39.316460);
+('Missão Velha', -7.243793, -39.145684);
+('Nova Olinda', -7.082457, -39.649827);
+('Porteiras', -7.522557, -39.114820);
+('Santana do Cariri', -7.380635, -39.723907);
+
+-- Inserção dos municípios da microrregião de Barro
+INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
+VALUES 
+('Aurora', -7.027792, -39.455540),
+('Barro', -7.176239, -39.744682),
+('Mauriti', -7.390730, -38.783710);
+
+-- Inserção dos municípios da microrregião de Chapada do Araripe
+INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
+VALUES 
+('Araripe', -7.220945, -40.076147),
+('Assaré', -6.924219, -39.850783),
+('Campos Sales', -6.941119, -40.249556),
+('Potengi', -7.066435, -40.249556),
