1968b2dafae11fe675ffcf1dbb06e50c1e172182
victor <141786730+vichsweet@users.noreply.github.com>
2024-03-31 22:26:28 -0300

    Update TrabalhoBD-Meso7.sql

--- atividades/atividade-03/TrabalhoBD-Meso7.sql
+++ atividades/atividade-03/TrabalhoBD-Meso7.sql
@@ -1,34 +1,44 @@
--- InserÁ„o dos municÌpios da microrregi„o de CaririaÁu
+-- Inser√ß√£o dos munic√≠pios da microrregi√£o de Cariria√ßu
 INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
 VALUES 
 ('Altaneira', -6.928323, -39.57252),
-('CaririaÁu', -7.047801, -39.283327),
+('Cariria√ßu', -7.047801, -39.283327),
 ('Farias Brito', -6.920674, -39.564316),
 ('Granjeiro', -7.145130, -39.336294);
 
--- InserÁ„o dos municÌpios da microrregi„o de Cariri
+-- Inser√ß√£o dos munic√≠pios da microrregi√£o de Cariri
 INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
 VALUES 
 ('Barbalha', -7.358413, -39.331816),
 ('Crato', -7.288420, -39.468317),
 ('Jardim', -7.581972, -39.280938),
 ('Juazeiro do Norte', -7.225012, -39.316460);
-('Miss„o Velha', -7.243793, -39.145684);
+('Miss√£o Velha', -7.243793, -39.145684);
 ('Nova Olinda', -7.082457, -39.649827);
 ('Porteiras', -7.522557, -39.114820);
 ('Santana do Cariri', -7.380635, -39.723907);
 
--- InserÁ„o dos municÌpios da microrregi„o de Barro
+-- Inser√ß√£o dos munic√≠pios da microrregi√£o de Barro
 INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
 VALUES 
 ('Aurora', -7.027792, -39.455540),
 ('Barro', -7.176239, -39.744682),
 ('Mauriti', -7.390730, -38.783710);
 
--- InserÁ„o dos municÌpios da microrregi„o de Chapada do Araripe
+-- Inser√ß√£o dos munic√≠pios da microrregi√£o de Chapada do Araripe
 INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
 VALUES 
 ('Araripe', -7.220945, -40.076147),
-('AssarÈ', -6.924219, -39.850783),
+('Assar√©', -6.924219, -39.850783),
 ('Campos Sales', -6.941119, -40.249556),
 ('Potengi', -7.066435, -40.249556),
+  
+-- Inser√ß√£o dos munic√≠pios da microrregi√£o de Brejo Santo 
+INSERT INTO TB_MUNICIPIOS (nome_municipio, latitude, longitude)
+VALUES 
+('Brejo Santo', -7.493684, -38.986306),
+('Abaiara', -7.3453, -39.0419),
+('Jati', -7.706011, -39.005734),
+('Milagre', -7.299983, -38.938225),
+('Penaforte', -7.810224, -39.070872);
+
